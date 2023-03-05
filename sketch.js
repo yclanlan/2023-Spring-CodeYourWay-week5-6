6e884159fd33ad41c9a073ebfa921db932b23fcc
Lan <lanlan.yichun@gmail.com>
2023-03-05 15:56:01 -0500

    black & white version

--- sketch.js
+++ sketch.js
@@ -8,14 +8,12 @@ let numLines = 1000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // background(255);
   strokeWeight(0.1);
   stroke(255);
 }
 
 function draw() {
-  colorMode(RGB)
- 
+  background(0,10);
   translate(width / 2, height / 2);
   rotate(frameCount/500);
   
@@ -24,34 +22,34 @@ function draw() {
   let increasing=10;
   variable+= increasing;
   
-  if ( variable < 0 || variable > 2000)   increasing *= -1;
-  // console.log(variable);
+  if (variable < 2000 || variable > 100)  increasing *= -1;
+  
   let variable1 = 5+variable/200*5;
   let variable2 = 5+variable/10*5;
-  scale(-2+sin(frameCount/300)*2);
-  background(map(variable,100,2000,0,255),10);
+  scale(.8+sin(frameCount/300)*2);
+
   // console.log(variable1);
 
   for (let i = 0; i < numLines; i++) {
 
     let x1 = sin((t + i) / variable1) * amplitude 
-           + cos((t + 1) / 5) * 50;
+          //  + cos((t + 1) / 5) * 50;
 
     let y1 = cos((-t + i) / variable1) * amplitude 
-           + sin(((t + 1) / 5) * 50);
+          //  + sin(((t + 1) / 5) * 50);
 
     let x2 = sin((t + i) / variable2) * (amplitude * 2) 
-           + cos(t + 1) * 50;
+          //  + cos(t + 1) * 50;
 
     let y2 = cos((-t + i) / variable2) * (amplitude * 2)
-           + sin(t + 1) * 50;
-    colorMode(HSB)
-    stroke(200,random(0,255),random(80,100));
+          //  + sin(t + 1) * 50;
+
+    stroke(random(200,255));
     line(x1, y1, x2, y2);
     
   }
 
-  t += 0.1;
+  t += 0.02;
   
 }
 
